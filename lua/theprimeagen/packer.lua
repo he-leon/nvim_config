84301f7d2f5e0b21b8ca8ff1bb4a9a3e808fdfb3
Leonhard Heizinger <leonhard.heizinger@heleon.de>
2022-12-22 21:30:55 +0100

    Added Black for Python formatting

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -27,7 +27,7 @@ return require('packer').startup(function(use)
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
-
+  use('psf/black')
   use {
 	  'VonHeikemen/lsp-zero.nvim',
 	  requires = {
--- to_be_ported_to_lua.vim
+++ to_be_ported_to_lua.vim
@@ -8,4 +8,5 @@ vnoremap % :s%^%\%%<cr>
 vnoremap -% :s/^%//g<cr>
 imap jj <Esc>
 nnoremap ,p iimport IPython; IPython.embed()
+autocmd FileType python nnoremap <Leader>f :Black<CR>
 
