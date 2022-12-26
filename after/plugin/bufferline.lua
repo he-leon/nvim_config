ff9e5e90f32df7345037314366865529e8d579c4
Leonhard Heizinger <leonhard.heizinger@heleon.de>
2022-12-26 18:13:00 +0100

    Ajdusted bufferline setup

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,2 +1,13 @@
 vim.opt.termguicolors = true
-require("bufferline").setup{}
+--require("bufferline").setup{}
+require("bufferline").setup({
+    highlights = {
+        buffer_selected = {
+            bg = '#373B41'
+        },
+        close_button_selected = {
+            bg = '#373B41'
+        },
+    }
+
+})
--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -17,8 +17,7 @@ vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
--- This is going to get me cancelled
-vim.keymap.set("i", "<C-c>", "<Esc>")
+
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
@@ -30,5 +29,7 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "qq", ":bd<CR>")
 
 vim.cmd 'source ~/.config/nvim/to_be_ported_to_lua.vim'
