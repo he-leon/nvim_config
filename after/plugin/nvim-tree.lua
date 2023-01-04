6ccffe97a05e040ddcac32e9bb9d4d81800bff1b
Leonhard Heizinger <leonhard.heizinger@heleon.de>
2023-01-04 16:18:57 +0100

    Adjusted nvim-tree; added gitignore

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -2,9 +2,12 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 require("nvim-tree").setup({
-  view = {
-    adaptive_size = true
-  }
+    view = {
+        adaptive_size = true
+    },
+    filters = {
+        dotfiles = true,
+    },
 })
 
 vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
