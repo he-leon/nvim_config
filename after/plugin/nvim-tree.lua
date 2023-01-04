85b34306a69607934d3abeb689fb0ee2ea2d77bd
Leonhard Heizinger <leonhard.heizinger@heleon.de>
2023-01-04 18:09:58 +0100

    Improved nvim-tree config

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -2,6 +2,11 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 require("nvim-tree").setup({
+    respect_buf_cwd = true,
+    update_focused_file =
+    {
+        enable = true,
+    },
     view = {
         adaptive_size = true
     },
