845de402e03d733a90d5e20f7f212b4a84c3a9cd
Leonhard Heizinger <leonhard.heizinger@heleon.de>
2023-01-04 22:09:14 +0100

    Improve buffer close shortcut

--- lua/heleon/remap.lua
+++ lua/heleon/remap.lua
@@ -30,6 +30,7 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "qq", ":bd<CR>")
+-- Closes buffer without closing window.
+vim.keymap.set("n", "qq", ":bp<bar>sp<bar>bn<bar>bd<CR>")
 
 vim.cmd 'source ~/.config/nvim/to_be_ported_to_lua.vim'
