cce1ce5e94ff41eb20704f1de7cc2eeea7c7ea27
Leonhard Heizinger <leonhard.heizinger@heleon.de>
2023-01-04 22:20:56 +0100

    Added Ctrl-s save remap

--- lua/heleon/remap.lua
+++ lua/heleon/remap.lua
@@ -18,14 +18,11 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
-
-vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+-- Format code
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
+-- Ctrl-s to save
+vim.keymap.set("n", "<C-s>", ":w<CR>")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
