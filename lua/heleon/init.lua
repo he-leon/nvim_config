d740b1398772c49c80cc89d37166e718a56fa317
Leonhard Heizinger <leonhard.heizinger@heleon.de>
2023-01-04 18:28:31 +0100

    renamed namespace to heleon

---
+++ lua/heleon/init.lua
@@ -0,0 +1,15 @@
+require("heleon.set")
+require("heleon.remap")
+
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
