d45a29846c37e182f34cc7a0bc7b6a02baa157c0
Leonhard Heizinger <leonhard.heizinger@heleon.de>
2024-10-26 20:37:32 +0200

    remove tsserver

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -1,7 +1,7 @@
 require("mason").setup()
 
 require("mason-lspconfig").setup {
-    ensure_installed = { "lua_ls", "rust_analyzer", "pyright", "clangd", "tsserver" },
+    ensure_installed = { "lua_ls", "rust_analyzer", "pyright", "clangd"},
 }
 
 require('mason-lspconfig').setup({
